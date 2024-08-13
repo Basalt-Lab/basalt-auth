d13407819586e01371ab1958083fcb41f2413ccc
ruby <necrelox@proton.me>
2024-08-13 11:27:43 +0200

    fix: catch json parse error when header or payload is not an object

--- Source/Common/Error/Enum/BasaltTokenErrorKeys.ts
+++ Source/Common/Error/Enum/BasaltTokenErrorKeys.ts
@@ -2,7 +2,9 @@
  * Represents the error codes for the Basalt token.
  */
 export enum BasaltTokenErrorKeys {
-    TOKEN_INVALID_STRUCTURE = 'error.domain.service.basaltToken.tokenInvalidStructure',
-    TOKEN_IS_EXPIRED = 'error.domain.service.basaltToken.tokenIsExpired',
-    TOKEN_SIGNATURE_INVALID = 'error.domain.service.basaltToken.tokenSignatureInvalid',
+    TOKEN_INVALID_STRUCTURE = 'error.domain.service.basaltToken.token_invalid_structure',
+    TOKEN_INVALID_HEADER = 'error.domain.service.basaltToken.token_invalid_header',
+    TOKEN_INVALID_PAYLOAD = 'error.domain.service.basaltToken.token_invalid_payload',
+    TOKEN_IS_EXPIRED = 'error.domain.service.basaltToken.token_is_expired',
+    TOKEN_SIGNATURE_INVALID = 'error.domain.service.basaltToken.token_signature_invalid',
 }
