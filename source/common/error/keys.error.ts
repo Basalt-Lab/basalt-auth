56c95b34637ee82d0e90c27371dba8b23d3dd424
ruby <necrelox@proton.me>
2024-09-20 15:53:58 +0200

    fix: bad export type in package json

--- source/common/error/keys.error.ts
+++ source/common/error/keys.error.ts
@@ -4,4 +4,4 @@ export const ErrorKeys = {
     TOKEN_INVALID_PAYLOAD: 'error.basalt-auth.token_invalid_payload',
     TOKEN_IS_EXPIRED: 'error.basalt-auth.token_is_expired',
     TOKEN_SIGNATURE_INVALID: 'error.basalt-auth.token_signature_invalid',
-}
+};
