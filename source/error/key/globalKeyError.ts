9d40e8d9d420cb33e510bbc331a2ba0240f26f3e
ruby <necrelox@proton.me>
2025-01-29 17:33:19 +0100

    refactor(🧹): update error message keys for basalt-auth localization

--- source/error/key/globalKeyError.ts
+++ source/error/key/globalKeyError.ts
@@ -2,9 +2,9 @@
  * Global error key is a list of errors in the global context.
  */
 export const GLOBAL_KEY_ERROR = {
-    TOKEN_INVALID_STRUCTURE: ['error.basalt-auth.token_invalid_structure', 401],
-    TOKEN_INVALID_HEADER: ['error.basalt-auth.token_invalid_header', 401],
-    TOKEN_INVALID_PAYLOAD: ['error.basalt-auth.token_invalid_payload', 401],
-    TOKEN_IS_EXPIRED: ['error.basalt-auth.token_is_expired', 401],
-    TOKEN_SIGNATURE_INVALID: ['error.basalt-auth.token_signature_invalid', 401]
+    TOKEN_INVALID_STRUCTURE: ['basalt-auth.error.token_invalid_structure', 401],
+    TOKEN_INVALID_HEADER: ['basalt-auth.error.token_invalid_header', 401],
+    TOKEN_INVALID_PAYLOAD: ['basalt-auth.error.token_invalid_payload', 401],
+    TOKEN_IS_EXPIRED: ['basalt-auth.error.token_is_expired', 401],
+    TOKEN_SIGNATURE_INVALID: ['basalt-auth.error.token_signature_invalid', 401]
 } as const;
